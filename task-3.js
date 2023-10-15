4d58b939f86f195b26871f89a4ecf6e5d85cce5d
GudvinPRO <138712942+GudvinPRO@users.noreply.github.com>
2023-10-15 15:48:02 +0300

    1

--- task-3.js
+++ task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return content + padding * 2 + border * 2;
+    return Numder.parseFloat(content) + Numder.parseFloat(padding) * 2 + Numder.parseFloat(border) * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
